ff8b2b62d9110ae306368b5c2d7408d59e40c8dd
monochandan <monochandan95@gmail.com>
2024-04-27 00:10:34 +0200

    commitig first time

---
+++ SeaBorn/libraries1.py
@@ -0,0 +1,14 @@
+### standard
+import numpy as np
+import pandas as pd
+from numpy.random import randn
+
+## stats
+
+from scipy import stats
+
+## plotting
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
